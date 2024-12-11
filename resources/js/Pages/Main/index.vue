66a759f8e6fbaf290e1365c2d92bb72453a9bfb8
nelzkie15-oss <67332045+nelzkie15-oss@users.noreply.github.com>
2024-12-12 02:38:56 +0800

    fd

---
+++ resources/js/Pages/Main/index.vue
@@ -0,0 +1,9 @@
+<<template>
+    <div>
+        <h2>My Main Page</h2>
+    </div>
+</template>
+
+<script setup>
+
+</script>
--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,21 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
-use Illuminate\Foundation\Application;
-use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
+use Illuminate\Support\Facades\Route;
+use Illuminate\Foundation\Application;
+use App\Http\Controllers\MainController;
+use App\Http\Controllers\ProfileController;
 
-Route::get('/', function () {
-    return Inertia::render('Welcome', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
-    ]);
-});
+// Route::get('/', function () {
+//     return Inertia::render('Welcome', [
+//         'canLogin' => Route::has('login'),
+//         'canRegister' => Route::has('register'),
+//         'laravelVersion' => Application::VERSION,
+//         'phpVersion' => PHP_VERSION,
+//     ]);
+// });
+
+Route::get('/', [MainController::class, 'Index'])->name('main.index');
 
 Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
