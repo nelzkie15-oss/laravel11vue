73568c7ef9cb3d286fd951666b3282cab64affb6
nelzkie15-oss <67332045+nelzkie15-oss@users.noreply.github.com>
2024-12-12 02:55:54 +0800

    fgh

---
+++ resources/js/Components/Navbar.vue
@@ -0,0 +1,23 @@
+<template>
+    <nav class="flex justify-between p-4 bg-gray-200">
+         <div class="text-lg">
+            Navbar
+         </div>
+       <ul class="flex mb-2 gap-x-4 me-auto mb-lg-lg">
+          <li class="nav-item">
+            <Link class="text-dark" :href="route('main.index')">Home</Link>
+          </li>
+          <li class="nav-item">
+            <Link class="text-dark" :href="route('main.aboutUs')">About</Link>
+          </li>
+          <li class="nav-item">
+            <Link class="text-dark" :href="route('main.contactUs')">Contact Us</Link>
+          </li>
+       </ul>
+    </nav>
+</template>
+
+<script setup>
+import {  Link } from '@inertiajs/vue3';
+
+</script>
