569f8ee7856f2c0ffbb1536399e028d12362e486
nelzkie15-oss <67332045+nelzkie15-oss@users.noreply.github.com>
2024-12-12 02:02:27 +0800

    dsf

--- public/index.php
+++ public/index.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 
+
 define('LARAVEL_START', microtime(true));
 // Determine if the application is in maintenance mode...
 if (file_exists($maintenance = __DIR__.'/../../laravel11vue/storage/framework/maintenance.php')) {
