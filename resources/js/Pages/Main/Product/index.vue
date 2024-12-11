bba50787f220aeb8999aead598da4fe8bd34de3e
nelzkie15-oss <67332045+nelzkie15-oss@users.noreply.github.com>
2024-12-12 04:23:13 +0800

    fgdfgd

--- resources/js/Pages/Main/Product/index.vue
+++ resources/js/Pages/Main/Product/index.vue
@@ -4,7 +4,7 @@
     <Nav>
         <div class="mx-4 mt-4">
              <h5>Product Lists</h5>
-             <Link :href="route('product.create')" class="p-3 mb-4 text-white bg-green-500 rounded">Add Product</Link>
+             <Link :href="route('products.create')" class="p-3 mb-4 text-white bg-green-500 rounded">Add Product</Link>
         </div>
     <div class="mx-4 mt-4">
         <table class="w-full bg-white border border-gray-200 shadow">
@@ -13,7 +13,7 @@
                     <td class="px-4 py-2 text-left border">Id</td>
                     <td class="px-4 py-2 text-left border">Name</td>
                     <td class="px-4 py-2 text-left border">Price</td>
-                    <!-- <td class="px-4 py-2 text-left border">Action</td> -->
+                   <td class="px-4 py-2 text-left border">Action</td>
                 </tr>
             </thead>
             <tbody>
@@ -21,7 +21,20 @@
                     <td class="px-4 py-2 border">1</td>
                     <td class="px-4 py-2 border">Apple</td>
                     <td class="px-4 py-2 border">140</td>
-                  
+                    <td>
+                        <Link :href="route('products.create')"
+                         class="inline-block p-3 px-2 py-1 mb-4 bg-blue-500 rounded me-2 text-dark">
+                         show</Link>
+
+                         <Link :href="route('products.create')"
+                         class="inline-block p-3 px-2 py-1 mb-4 bg-orange-500 rounded me-2 text-dark">
+                         edit</Link>
+
+                         <Link :href="route('products.create')"
+                         class="inline-block p-3 px-2 py-1 mb-4 bg-red-500 rounded me-2 text-dark">
+                         delete</Link>
+                    </td>
+
                 </tr>
             </tbody>
 
