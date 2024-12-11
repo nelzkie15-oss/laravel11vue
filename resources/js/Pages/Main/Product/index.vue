f0108cbb5243f623fedf056c918f485426931c68
nelzkie15-oss <67332045+nelzkie15-oss@users.noreply.github.com>
2024-12-12 04:08:19 +0800

    gfh

--- resources/js/Pages/Main/Product/index.vue
+++ resources/js/Pages/Main/Product/index.vue
@@ -4,8 +4,42 @@
     <Nav>
         <div class="mx-4 mt-4">
              <h5>Product Lists</h5>
-             <Link :href="route('prodcut.create')">Add Product</Link>
+             <Link :href="route('product.create')" class="p-3 mb-4 text-white bg-green-500 rounded">Add Product</Link>
         </div>
+        <table class="w-full bg-white border border-gray-200 shadow">
+            <thead>
+                <tr>
+                    <td class="px-4 py-2 text-left border">Id</td>
+                    <td class="px-4 py-2 text-left border">Name</td>
+                    <td class="px-4 py-2 text-left border">Price</td>
+                    <td class="px-4 py-2 text-left border">Action</td>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td class="px-4 py-2 border">1</td>
+                    <td class="px-4 py-2 border">Apple</td>
+                    <td class="px-4 py-2 border">1</td>
+                    <td class="px-4 py-2 border">140</td>
+                    <td>
+                        <Link :href="route('product.create')"
+                         class="inline-block p-3 px-2 py-1 mb-4 bg-blue-500 rounded me-2 text-dark">
+                         show</Link>
+
+                         <Link :href="route('product.create')"
+                         class="inline-block p-3 px-2 py-1 mb-4 bg-orange-500 rounded me-2 text-dark">
+                         edit</Link>
+
+                         <Link :href="route('product.create')"
+                         class="inline-block p-3 px-2 py-1 mb-4 bg-red-500 rounded me-2 text-dark">
+                         delete</Link>
+                    </td>
+
+                </tr>
+            </tbody>
+
+
+        </table>
     </Nav>
   </div>
 </template>
