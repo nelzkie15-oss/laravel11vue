48faccf5de83312782098e565cebac7aecfc022c
nelzkie15-oss <67332045+nelzkie15-oss@users.noreply.github.com>
2024-12-13 02:54:51 +0800

    sdf

--- resources/js/Pages/Main/Product/index.vue
+++ resources/js/Pages/Main/Product/index.vue
@@ -42,7 +42,7 @@
 
                 <button
                   type="submit"
-                  class="px-5 py-2 mb-4 text-white bg-red-600 rounded"
+                  class="inline-block p-3 px-2 py-1 mt-3 mb-4 ml-2 text-white bg-red-600 rounded"
                 >
                   Delete
                 </button>
