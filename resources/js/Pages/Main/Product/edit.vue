144b14a384885073d19556d9d614df8e9eceaa4d
nelzkie15-oss <67332045+nelzkie15-oss@users.noreply.github.com>
2024-12-13 03:31:55 +0800

    dg

--- resources/js/Pages/Main/Product/edit.vue
+++ resources/js/Pages/Main/Product/edit.vue
@@ -42,9 +42,11 @@
 
                 <button
                   type="submit"
+                   :disabled="form.processing"
                   class="px-5 py-2 mb-4 text-white bg-red-600 rounded"
                 >
-                  Update
+                <span v-if="form.processing">Update...</span>
+                <span v-else>Update</span>
                 </button>
               </div>
             </div>
