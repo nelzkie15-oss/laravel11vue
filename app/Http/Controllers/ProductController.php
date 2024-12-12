e8566a710361f24a2804f957d14446a0bb2ccecf
nelzkie15-oss <67332045+nelzkie15-oss@users.noreply.github.com>
2024-12-13 03:43:42 +0800

    sdf

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -80,8 +80,10 @@ class ProductController extends Controller
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(Product $product)
     {
-        //
+        $product->delete();
+
+        return redirect()->to('/products')->with('message', 'Product Deleted Successfully');
     }
 }
