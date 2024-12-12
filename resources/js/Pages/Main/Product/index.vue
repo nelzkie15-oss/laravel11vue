3e70d27c275063b177a3095e4bcee8403d9a8ea0
nelzkie15-oss <67332045+nelzkie15-oss@users.noreply.github.com>
2024-12-13 02:50:53 +0800

    dsf

--- resources/js/Pages/Main/Product/index.vue
+++ resources/js/Pages/Main/Product/index.vue
@@ -30,9 +30,12 @@
                          class="inline-block p-3 px-2 py-1 mt-3 mb-4 text-white bg-orange-500 rounded me-2">
                          edit</Link>
 
-                         <Link :href="route('products.create')"
-                         class="inline-block p-3 px-2 py-1 mt-3 mb-4 text-white bg-red-500 rounded me-2">
-                         delete</Link>
+                         <button
+                        type="submit"
+                        class="px-5 py-2 mb-4 text-white bg-red-600 rounded"
+                        >
+                        Delete
+                        </button>
                     </td>
 
                 </tr>
