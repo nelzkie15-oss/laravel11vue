70c871989bec947ddb36784ab5e44e7a1de54529
nelzkie15-oss <67332045+nelzkie15-oss@users.noreply.github.com>
2024-12-13 02:15:14 +0800

    fg

--- resources/js/Pages/Main/Product/create.vue
+++ resources/js/Pages/Main/Product/create.vue
@@ -2,11 +2,13 @@
     <div>
        <Head title="Create Product"/>
        <Nav>
-           <div class="mx-4 mt-4">
+        <div class="mx-4 mt-4">
+           <div class="flex justify-between">
                 <h5 class="mb-2">Create Product</h5>
-                <Link :href="route('products.index')" class="p-2 mt-3 mb-4 text-white rounded bg-danger-500 float-end ">Back</Link>
+                <Link :href="route('products.index')" class="p-2 mt-3 mb-4 text-white bg-red-500 rounded float-end ">Back</Link>
            </div>
-       <div class="grid grid-cols-12 gap-4">
+
+       <div class="grid grid-cols-12 gap-4 ">
            <div class="col-span-3">
               <div class="mb-3">
                   <label for="Product Name">Product Name</label>
@@ -18,18 +20,26 @@
               </div>
 
               <div class="mb-3">
-                  <Link :href="route('products.index')" class="p-2 mt-3 mb-4 text-white bg-red-500 rounded float-end ">Back</Link>
-              </div>
+                  <Link :href="route('products.index')"
+                  class="inline-block px-5 py-2 mb-4 mr-2 text-white bg-red-600 rounded">Back</Link>
+
                <button type="submit"
-               class="p-2 px-5 mt-3 mb-4 text-white bg-blue-500 rounded">
+               class="px-5 py-2 mb-4 text-white bg-blue-600 rounded">
                  Save
                </button>
+            </div>
            </div>
+        </div>
        </div>
        </Nav>
      </div>
    </template>
    <script setup>
      import Nav from "@/Layouts/MainLayout.vue";
-     import { Head, Link } from "@inertiajs/vue3";
+     import { Head, Link, useForm } from "@inertiajs/vue3";
+
+     const form = useForm({
+        name: '',
+        price: ''
+     });
    </script>
