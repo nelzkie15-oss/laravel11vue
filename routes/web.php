4b81a2afdabbea631c4aa3c47758a3d693fe5df6
nelzkie15-oss <67332045+nelzkie15-oss@users.noreply.github.com>
2024-12-13 16:22:33 +0800

    xzc

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,16 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
-use Illuminate\Foundation\Application;
-use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
+use Illuminate\Support\Facades\Route;
+use Illuminate\Foundation\Application;
+use App\Http\Controllers\MainController;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\ProfileController;
 
-Route::get('/', function () {
-    return Inertia::render('Welcome', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
-    ]);
-});
+Route::get('/about', [MainController::class, 'aboutUs'])->name('main.aboutUs');
+Route::get('/contactus', [MainController::class, 'contactUs'])->name('main.contactUs');
+
+Route::resource('/products', ProductController::class);
 
 Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
